964a84d12611bdd4fce33b73d9cc880c1918cd9b
AndyLazy <97122165+AndyLazy@users.noreply.github.com>
2023-10-13 03:06:36 +0000

    changes 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,18 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to MARAZMAI
+# Welcome to MARAZMAI 1
+
 """
 
-result = divmod(8, 3)
-print('Quotient and Remainder = ',result)
+
+result = divmod(10, 6)
+# print('Quotient and Remainder = ',result)
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of points in spiral", 1, 500, 100)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 50)
 
     Point = namedtuple('Point', 'x y')
     data = []
